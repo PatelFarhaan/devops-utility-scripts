217b252684ac927fb7791f4afb0be3a247dbc8ac
Farhaan <patel.farhaaan@gmail.com>
2021-06-08 19:05:04 -0700

    plugins installation added in jenkins service script

--- jenkins_service.sh
+++ jenkins_service.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 
+jenkins_dir=/home/ubuntu/data/jenkins
+plugin_dir="$jenkins_dir/plugins"
+list_of_plugins_to_download=(
+  "aws-java-sdk"
+  "codedeploy"
+)
+
 sudo apt-get update && sudo apt-get upgrade -y &&
 
 sudo apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y &&
@@ -21,13 +28,37 @@ sudo docker run \
   -p 8081:8080 \
   --name jenkins \
   --restart always \
-  -v `pwd`/data/jenkins:/var/jenkins_home \
+  -v "$jenkins_dir":/var/jenkins_home \
   -v /var/run/docker.sock:/var/run/docker.sock \
   jenkinsci/blueocean &&
 
-while [ ! `sudo ls /home/ubuntu/data/jenkins/secrets/initialAdminPassword` ];
+while [ ! "$(sudo ls "$jenkins_dir/secrets/initialAdminPassword")" ];
 do
-    sleep 15
+    sleep 20
 done
 
-sudo cat /home/ubuntu/data/jenkins/secrets/initialAdminPassword
+
+download_plugin() {
+  if [[ -f $(sudo ls "$plugin_dir/$1.hpi") ]] || [[ -f $(sudo ls "$plugin_dir/$1.jpi") ]]; then
+    echo "Skipped: $1 (already installed)"
+    return 0
+  else
+    echo "Installing: $1"
+    sudo wget -P "$plugin_dir" "https://updates.jenkins-ci.org/download/plugins/$1/latest/$1.hpi"
+    return 0
+  fi
+}
+
+
+for plugin in "${list_of_plugins_to_download[@]}"
+do
+  download_plugin "$plugin"
+done
+
+sudo docker restart jenkins &&
+
+sudo cat "$jenkins_dir/secrets/initialAdminPassword"
+
+
+
+# Name of plugins to download: https://plugins.jenkins.io/
